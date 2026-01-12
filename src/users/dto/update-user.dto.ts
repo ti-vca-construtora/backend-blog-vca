9abee2816acae466c39556d27f634fd824e3acdc
Jorge Jesus Santos <jorge.santos@vcaconstrutora.com.br>
2026-01-12 14:52:01 -0300

    atualizacao dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,38 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator'
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto{
-      @ApiProperty({
-        example: 'João da Silva',
-        description: 'Nome completo do usuário',
-      })
-      @IsString()
-      @IsOptional()
-      name: string
-    
-      @ApiProperty({
-        example: 'joao@email.com',
-        description: 'Email do usuário (único)',
-      })
-      @IsEmail()
-      @IsOptional()
-      email: string
-    
-      @ApiProperty({
-        example: '123456',
-        description: 'Senha do usuário (mínimo 6 caracteres)',
-      })
-      @IsString()
-      @MinLength(6)
-      @IsOptional()
-      password: string
-    
-      @ApiPropertyOptional({
-        example: 'Admin',
-        description: 'Perfil do usuário (ex: Admin, Editor, User)',
-      })
-      
-      @IsOptional()
-      @IsString()
-      role?: string
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
