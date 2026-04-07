41b22aa332dc9300c99e433f94870a8739e28cbc
jsmaximo <144278242+ti-vca-construtora@users.noreply.github.com>
2026-04-07 14:06:03 -0300

    fix: resolve conflitos

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
@@ -6,13 +5,5 @@ import { PrismaService } from './prisma.service';
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
-=======
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  providers: [PrismaService],
-  exports:[PrismaService]
->>>>>>> c6ded7540776d2011921a19c4d062448741fb538
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
@@ -8,15 +7,3 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 }
-=======
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
-
-@Injectable()
-export class PrismaService extends PrismaClient {
-    async enableShutdownHooks() {
-    await this.$disconnect()
-  }
-}
->>>>>>> c6ded7540776d2011921a19c4d062448741fb538
